c3fa15b8e4f4394a8ac6dcca1082a0bc36ded432
Jithin Renny <youngdetective222b@gmail.com>
2025-03-15 10:37:09 +0530

    Feature- Base View controller for design.

---
+++ AskMeAnything/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AskMeAnything
+//
+//  Created by Jithin Renny on 13/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
